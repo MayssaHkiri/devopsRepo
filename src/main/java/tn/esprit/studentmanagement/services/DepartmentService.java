3395f89a10fce13880c952c4aca04e3f11a123e7
MayssaHkiri <may26saa@gmail.com>
2026-06-11 18:11:10 +0200

    fix departementService issues

--- src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/DepartmentService.java
@@ -7,11 +7,22 @@ import tn.esprit.studentmanagement.repositories.DepartmentRepository;
 
 import java.util.List;
 
-@Service
+package tn.esprit.studentmanagement.services;
 
+import org.springframework.stereotype.Service;
+import tn.esprit.studentmanagement.entities.Department;
+import tn.esprit.studentmanagement.repositories.DepartmentRepository;
+
+import java.util.List;
+
+@Service
 public class DepartmentService implements IDepartmentService {
-    @Autowired
-    DepartmentRepository departmentRepository;
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
 
     @Override
     public List<Department> getAllDepartments() {
@@ -20,7 +31,10 @@ public class DepartmentService implements IDepartmentService {
 
     @Override
     public Department getDepartmentById(Long idDepartment) {
-        return departmentRepository.findById(idDepartment).get();
+        return departmentRepository.findById(idDepartment)
+                .orElseThrow(() ->
+                        new RuntimeException("Department not found with id: " + idDepartment)
+                );
     }
 
     @Override
@@ -30,6 +44,6 @@ public class DepartmentService implements IDepartmentService {
 
     @Override
     public void deleteDepartment(Long idDepartment) {
-departmentRepository.deleteById(idDepartment);
+        departmentRepository.deleteById(idDepartment);
     }
 }
